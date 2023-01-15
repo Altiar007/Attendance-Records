b4aee70bb89daac51ccde07f7b6f66107112c7e5
moksh <altiarahad@gmail.com>
2023-01-15 14:59:18 +0530

    file to read/write data

---
+++ file_handler.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    return 0;
+}
+
+void write_subject_name{
+    fstream Subjects;
+    subjects.open("Subjects",ios::out){
+        Subjects << sub_name << sed::endl;
+    }
+}
