6967b8c12d0654a595474f925ac921bbb91574a6
moksh <altiarahad@gmail.com>
2023-01-15 14:59:44 +0530

    bugs added, to be fixed

--- main.cpp
+++ main.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <string>
+#include "file_handler.h"
 
 //Creating a class for lecture details
 class lecture{
     std::string subject_name;
     public:
-    void getdata(void);
+    void getdata(std::string);
     void display(void);
 };
 
 //function to get data of the class
-void lecture::getdata(void) {
+void lecture::getdata(std::string s_name) {
     s_name = subject_name;
 }
 
 //function to display data of the class
 void lecture::display(void) {
-    std::cout << "Subject Name: " << subject_name<< std::endl;
+    // std::cout << "Subject Name: " << subject_name<< std::endl;
 }
 
 int main() {
-    int n{};
     std::string sub_name;
+    int n{};
     std::cout << "Enter the Number of Subjects You want to monitor attendance of: " << std::endl;
     std::cin >> n;
     for(int i = 1; i<n+1; i++){
         std::cout << "Enter the name of the " <<i <<"th Subject"<<std::endl;
         std:: cin >> sub_name;
         lecture sub_name;
-        sub_name.getdata();
+        write_subject_name();
+        sub_name.getdata(sub_name);
         sub_name.display();
     }
     return 0;
