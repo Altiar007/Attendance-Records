945e42844206a16f5c88252621cbaeeaa59f0fc4
moksh <altiarahad@gmail.com>
2023-01-16 16:41:12 +0530

    added switch cases and functions

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 #include <fstream>
 
 std::string sub_name;
+std::fstream Subjects;
 
 // Input Stream
 void input() {
     std::cout << "[+] Enter the name of the Subject" << std::endl;
-    std::getline(std::cin, sub_name);
+    // std::getline(std::cin, sub_name);
+    std::cin >> sub_name;
 }
 
 // Output Stream
@@ -17,7 +19,6 @@ void output() {
 
 // Write subject name to file
 void write_subject_name(std::string s_name) {
-    std::fstream Subjects;
     Subjects.open("Subjects", std::ios::app); //app for appending
     if(Subjects.is_open()) {
         Subjects << s_name << std::endl;
@@ -31,12 +32,42 @@ void read_subject_name() {
     Subjects.open("Subjects", std::ios::in); //read mode
     if(Subjects.is_open()) {
         std::string line;
+        while(std::getline(Subjects, line)) {
+            std::cout << line << std::endl;
+        }
+        Subjects.close();
     }
 }
 
 int main() {
-    input();
-    output();
-    write_subject_name(sub_name);
+    char f_input, s_input;
+    std::cout << "Welcome to Attendance Records Program" << std::endl;
+    std::cout << "This Piece of Software Lets you Monitor your attendance" << std::endl;
+    std::cout << "Press M to enter Menu" << std::endl;
+    std::cout << "Press E to Exit The Program" << std::endl;
+    std::cin >> f_input;
+
+    switch (f_input) {
+        case 'M':
+            std::cout << "Press A to add a Subject" << std::endl;
+            std::cout << "Press D to Display added Subjects" << std::endl;
+            std::cout << "Press E to Exit Menu" << std::endl;
+            // std::cout << "Press A to add a Subject" << std::endl;
+            std::cin >> s_input;
+
+            switch (s_input) {
+                case 'A':
+                input();
+                write_subject_name(sub_name);
+            
+            case 'D':
+                output();
+            
+            case 'E':
+                break;
+            }
+        case 'E':
+        break;
+    }
     return 0;
 }
