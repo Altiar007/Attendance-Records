c06eda0dafb35b913a02d2fad8fddd82af3f17c9
moksh <altiarahad@gmail.com>
2023-01-17 20:42:06 +0530

    tried fixing, didn't work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <fstream>
 #include <windows.h>
 #include "file_handler.h"
 
@@ -9,8 +8,9 @@ std::string sub_name;
 // Input Stream
 void input() {
     std::cout << "[+] Enter the name of the Subject" << std::endl;
-    // std::getline(std::cin, sub_name);
+    // getline(std::cin, sub_name);
     std::cin >> sub_name;
+    write_subject_name(sub_name);
 }
 
 // Output Stream
@@ -19,37 +19,40 @@ void output() {
 }
 
 int main() {
-    char f_input, s_input;
+    char f_input, s_input;// first input, second input
     Sleep(500);
     std::cout << "Welcome to Attendance Records Program" << std::endl;
     Sleep(500);
-    std::cout << "This Piece of Software Lets you Monitor your attendance" << std::endl;
+    std::cout << "This Software Lets you Monitor your Attendance" << std::endl;
     Sleep(500);
     std::cout << "Press M to enter Menu" << std::endl;
+    std::cout << "Press H for Help Menu" << std::endl;
     std::cout << "Press E to Exit The Program" << std::endl;
     std::cin >> f_input;
 
     switch (f_input) {
-        case 'M':
+        case 'M'://main menu
             std::cout << "Press A to add a Subject" << std::endl;
             std::cout << "Press D to Display added Subjects" << std::endl;
             std::cout << "Press E to Exit Menu" << std::endl;
-            // std::cout << "Press A to add a Subject" << std::endl;
             std::cin >> s_input;
 
             switch (s_input) {
                 case 'A':
-                input();
-                write_subject_name(sub_name);
+                    input();
+                    // write_subject_name(sub_name);
             
-            case 'D':
-                output();
-            
-            case 'E':
-                break;
+                case 'D':
+                    output();
+                
+                case 'E':
+                    break;
             }
-        case 'E':
-        break;
+        
+        case 'H'://Help Text
+
+        case 'E'://Exit Program
+            break;
     }
     return 0;
 }
