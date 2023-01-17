7dcb620dc76f91c55a7fba3f55c24d0a2f265931
moksh <altiarahad@gmail.com>
2023-01-17 09:36:52 +0530

    fixed bugs and added file-handling part

--- file_handler.h
+++ file_handler.h
@@ -1,13 +1,32 @@
+#ifndef FILE_HANDLER_H
+#define FILE_HANDLER_H
+
 #include <iostream>
 #include <fstream>
 
-int main() {
-    return 0;
+std::fstream Subjects;
+
+
+// Write subject name to file
+void write_subject_name(std::string s_name) {
+    Subjects.open("Subjects", std::ios::app); //app for appending
+    if(Subjects.is_open()) {
+        Subjects << s_name << std::endl;
+        Subjects.close();
+    }
+    std::cout << "[^] Subject Added and Written to File" << std::endl;
 }
 
-void write_subject_name{
-    fstream Subjects;
-    subjects.open("Subjects",ios::out){
-        Subjects << sub_name << sed::endl;
+// Read Subject Name from the File
+void read_subject_name() {
+    Subjects.open("Subjects", std::ios::in); //read mode
+    if(Subjects.is_open()) {
+        std::string line;
+        while(std::getline(Subjects, line)) {
+            std::cout << line << std::endl;
+        }
+        Subjects.close();
     }
 }
+
+#endif
