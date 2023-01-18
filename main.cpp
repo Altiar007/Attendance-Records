26d37c7e0c84f596b93b7ddd505a2347d41d3677
moksh <altiarahad@gmail.com>
2023-01-18 11:03:11 +0530

    added more functions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,20 @@
 
 std::string sub_name;
 
+// Display Options
+void display_options(){
+    std::cout << "Press M to enter Menu" << std::endl;
+    std::cout << "Press H for Help Menu" << std::endl;
+    std::cout << "Press E to Exit The Program" << std::endl;
+}
+
+// Display Menu
+void display_menu() {
+    std::cout << "Press A to add a Subject" << std::endl;
+    std::cout << "Press D to Display added Subjects" << std::endl;
+    std::cout << "Press E to Exit Menu" << std::endl;
+}
+
 // Input Stream
 void input() {
     std::cout << "[+] Enter the name of the Subject" << std::endl;
@@ -19,22 +33,18 @@ void output() {
 }
 
 int main() {
-    char f_input, s_input;// first input, second input
+    char f_input = 'A', s_input = 'A';// first input, second input
     Sleep(500);
     std::cout << "Welcome to Attendance Records Program" << std::endl;
     Sleep(500);
     std::cout << "This Software Lets you Monitor your Attendance" << std::endl;
     Sleep(500);
-    std::cout << "Press M to enter Menu" << std::endl;
-    std::cout << "Press H for Help Menu" << std::endl;
-    std::cout << "Press E to Exit The Program" << std::endl;
+    display_options();
     std::cin >> f_input;
 
     switch (f_input) {
         case 'M'://main menu
-            std::cout << "Press A to add a Subject" << std::endl;
-            std::cout << "Press D to Display added Subjects" << std::endl;
-            std::cout << "Press E to Exit Menu" << std::endl;
+            display_menu();
             std::cin >> s_input;
 
             switch (s_input) {
